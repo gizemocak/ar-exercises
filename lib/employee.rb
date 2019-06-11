b93ff84d00bccde0498dd372702986aaeb48d507
gizemocak <gizem_ocak@outlook.com>
2019-06-11 23:32:44 +0000

    Add a callback

--- lib/employee.rb
+++ lib/employee.rb
@@ -6,4 +6,13 @@ class Employee < ActiveRecord::Base
   validates :hourly_rate, numericality: { greater_than: 40 }
   validates :hourly_rate, numericality: { less_than: 200 }
   validates :store, presence: true
+
+  before_create do
+    self.password = generate_code(8)
+  end
+
+  def generate_code(number)
+    charset = Array('A'..'Z') + Array('a'..'z')
+    Array.new(number) { charset.sample }.join
+  end
 end
