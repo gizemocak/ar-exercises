8bff07012da614acb66ca7ab404bcd7f9773ef1d
gizemocak <gizem_ocak@outlook.com>
2019-06-11 20:36:23 +0000

    chain together where and count

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+#total revenue for the entire company (all stores)
+puts Store.sum(:annual_revenue)
+#the average annual revenue for all stores
+puts Store.average(:annual_revenue)
+
+puts Store.where('annual_revenue >= ?', [1000000]).count
+
