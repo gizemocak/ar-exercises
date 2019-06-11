9ce9f555d9e59d54d71447da9860bcd3bdfa6a8b
gizemocak <gizem_ocak@outlook.com>
2019-06-11 21:10:24 +0000

    One-to-many association

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,9 +6,9 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-store4 = Store.create(name: 'Surrey', annual_revenue:224000, mens_apparel:false, womens_apparel: true)
-store5 = Store.create(name: 'Whistler', annual_revenue:1900000, mens_apparel:true, womens_apparel: false)
-store6 = Store.create(name: 'Yaletown', annual_revenue:430000, mens_apparel:true, womens_apparel: true)
+@store4 = Store.create(name: 'Surrey', annual_revenue:224000, mens_apparel:false, womens_apparel: true)
+@store5 = Store.create(name: 'Whistler', annual_revenue:1900000, mens_apparel:true, womens_apparel: false)
+@store6 = Store.create(name: 'Yaletown', annual_revenue:430000, mens_apparel:true, womens_apparel: true)
 
 #stores that sell man apparel
 @mens_stores = Store.where(mens_apparel:true)
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -1,5 +1,5 @@
 require_relative '../setup'
-require_relative './exercise_1'
+require_relative './exercise_2'
 require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
@@ -8,4 +8,13 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Boo", hourly_rate: 60)
+@store1.employees.create(first_name: "Alex", last_name: "Vir", hourly_rate: 50)
+@store1.employees.create(first_name: "Ken", last_name: "Apple", hourly_rate: 35)
+
+@store2.employees.create(first_name: "Marry", last_name: "Mar", hourly_rate: 60)
+@store2.employees.create(first_name: "Bobby", last_name: "Bee", hourly_rate: 60)
+@store2.employees.create(first_name: "Landon", last_name: "Lio", hourly_rate: 60)
+@store2.employees.create(first_name: "Carla", last_name: "Beet", hourly_rate: 60)
+@store2.employees.create(first_name: "Kemmy", last_name: "Bonn", hourly_rate: 60)
